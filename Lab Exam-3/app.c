efd52d97a3f07487e912a9fb00e4964d89f6dfe7
sajjadbhuiyan <sajjadbhuiyan2018@gmail.com>
2022-05-05 22:35:20 +0600

    I done

--- Lab Exam-3/app.c
+++ Lab Exam-3/app.c
@@ -1,9 +1,22 @@
 #include<stdio.h>
 int main(){
+
     
     return 0;
 }
 
+
+/* 
+A :
+char  a[1], b[1], c[1];
+    scanf("%s %s %s", &a, &b, &c);
+    char x[3] = a[1]+b[1]+c[1];
+    printf("%s", a);
+    int x = atoi(a);
+    int y = atoi(b);
+    int z = atoi(c); 
+    
+*/
 
 /* 
 B :
@@ -69,4 +82,28 @@ int n;
 
     // Idleness limit exceeded on test 1
 
+ */
+
+
+/* 
+I :
+
+int X;
+    scanf("%d", &X);
+    if (X >= 0 && X <= 100 )
+        {
+            if (X < 40)
+        {
+            printf("%d", 40 - X);
+        }else if (X >= 40 && X < 70)
+        {
+            printf("%d", 70 - X);
+        }else if (X >= 70 && X < 90)
+        {
+            printf("%d", 90 - X);
+        }else if (X >= 90)
+        {
+            printf("expert");
+        }  
+    }
  */
